7bf05130070d431f3eb3096f8f68f487bc2325fc
ggandur <grgandur@gmail.com>
2023-04-02 23:24:10 -0300

    Adding viewController to control what view to display

--- MyApp.swift
+++ MyApp.swift
@@ -1,12 +1,26 @@
+import Foundation
 import SwiftUI
+
+class AppSettings: ObservableObject {
+    @Published var viewController: Int = 0
+}
 
 @main
 struct MyApp: App {
+    @StateObject var appSettings = AppSettings()
+
     var body: some Scene {
         WindowGroup {
-//            DoublePendulumView()
-//            DoublePendulumDemonstrationView()
-            ScientistView()
+            switch appSettings.viewController {
+            case 0:
+                ScientistView().environmentObject(appSettings)
+            case 1:
+                DoublePendulumDemonstrationView()
+            case 2 :
+                DoublePendulumView()
+            default:
+                ScientistView().environmentObject(appSettings)
+            }
         }
     }
 }
--- ScientistView.swift
+++ ScientistView.swift
@@ -5,24 +5,31 @@
 //  Created by Gabriel Gandur on 02/04/23.
 //
 
+import Foundation
 import SwiftUI
 
 struct ScientistView: View {
+    @EnvironmentObject var appSettings: AppSettings
     @State var tapsCounter: Int = 0
+    
     var body: some View {
-        ZStack{
+        ZStack {
             Color(red: 0.85, green: 0.65, blue: 0.95)
                 .ignoresSafeArea()
             Text("You tapped \(tapsCounter) times!")
         }
         .onTapGesture {
             tapsCounter += 1
+            
+            if tapsCounter == 5 {
+                appSettings.viewController = 1
+            }
         }
     }
 }
 
 struct ScientistView_Previews: PreviewProvider {
     static var previews: some View {
-        ScientistView()
+        ScientistView().environmentObject(AppSettings())
     }
 }
